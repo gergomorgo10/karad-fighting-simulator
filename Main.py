109284a25d8cab9d5766ca919c9b42dafb238cf8
szaklajda_gergo <szgeri39@gmail.com>
2024-03-04 09:18:01 +0100

    made working username choosing system

--- Main.py
+++ Main.py
@@ -1,3 +1,16 @@
 import random
 
 print("Welcom to karád simulator")
+
+name = input("what is your username? : ")
+decision = int(input("are you sure? (1 yes , 2 no)  :"))
+while decision <= 0 or decision > 2:
+    print("nope")
+    decision = int(input("are you sure? (1 yes , 2 no)  :"))
+while decision != 1:
+    if decision == 2:
+        name = input("what is your username? : ")
+        decision = int(input("are you sure? (1 yes , 2 no)  :"))
+        while decision <= 0 or decision > 2:
+            print("nope")
+            decision = int(input("are you sure? (1 yes , 2 no)  :"))
