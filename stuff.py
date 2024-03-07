91d2b0f2a3ecbd0c1d505e581a2f69ad4ab9734a
szaklajda_gergo <szgeri39@gmail.com>
2024-03-07 12:02:59 +0100

    basic enemy spawner made

--- stuff.py
+++ stuff.py
@@ -1,3 +1,4 @@
+import random
 class Game:
     def __init__(self, name, hp, speed, dmg):
         self.Name = name
@@ -7,3 +8,26 @@ class Game:
 
     def Kiir(self):
         print(f"your name is {self.Name}\nStats: dmg = {self.Dmg},\thp = {self.Hp},\tspeed = {self.Speed}")
+
+    def Enemygenerator(self):
+        ranszam = random.randint(1, 100000)
+        if ranszam >= 1 and ranszam < 5000:
+            print("mugger has appeared")
+            return 1
+        elif ranszam >= 5000 and ranszam < 10000:
+            print("kindergarten kid has appeared")
+            return 2
+        elif ranszam >= 10000 and ranszam < 30000:
+            print("kid gypsy has appeared")
+            return 3
+        elif ranszam >= 30000 and ranszam < 66666:
+            print("adult gypsy has appeared")
+            return 4
+        elif ranszam == 66666:
+            print("D2 has appeared")
+            return 5
+        elif ranszam > 66666 and ranszam <= 100000:
+            print("banklifter has appeared")
+            return 6
+
+
